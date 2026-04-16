76e2c94cc063297d8a1f4c1b88450aafbfc398a2
Nabeelfarooqi <nfarooqi090@gmail.com>
2026-04-15 21:54:31 -0400

    Fix CORS for production

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(cors())
+app.use(cors({
+    origin: '*'
+}))
 app.use(express.json({ limit: '10mb' }))
 
 const anthropic = new Anthropic({
